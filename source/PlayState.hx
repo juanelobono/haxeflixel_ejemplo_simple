57463ae9e9a003787bf94ddb0294ec25e07b00bc
Pablo Abratte <pabratte@gmail.com>
2017-09-09 20:23:17 -0300

    Agregar un sprite a la escena

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,18 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState{
 	override public function create():Void{
 		super.create();
+		personaje = new FlxSprite(10, 10, "assets/images/char_wbg.png");
+		add(personaje);
 	}
 
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
 	}
+
+	var personaje: FlxSprite;
 }
