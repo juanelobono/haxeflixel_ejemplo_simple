c3a83db6561fb6a25eec94b7f55bccd40b86667c
Pablo Abratte <pabratte@gmail.com>
2017-09-09 22:17:18 -0300

    Mover al personaje con el teclado

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,16 +2,38 @@ package;
 
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.FlxG;
 
 class PlayState extends FlxState{
 	override public function create():Void{
 		super.create();
-		personaje = new FlxSprite(10, 10, "assets/images/char_wbg.png");
+		personaje = new FlxSprite(152, 152, "assets/images/char_wbg.png");
 		add(personaje);
 	}
 
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
+
+		// mover al personaje con las teclas
+		if(FlxG.keys.pressed.A){
+			personaje.velocity.x = -100;
+		}else if(FlxG.keys.pressed.D){
+			personaje.velocity.x = 100;
+		}else{
+			personaje.velocity.x = 0;
+		}
+
+		// comprobar que no se salga de la pantalla
+		if(personaje.x < 0){
+			personaje.x = 0;
+			personaje.velocity.x = 0;
+		}
+		if(personaje.x > 304){
+			personaje.x = 304;
+			personaje.velocity.x = 0;
+		}
+
+
 	}
 
 	var personaje: FlxSprite;
